c7e787db4a4296322c07d6e206ed0ece90596d5f
Vitalii Derda <derdavitaliy@gmail.com>
2022-11-16 10:14:23 +0200

    fix modificators

--- Sources/ModificatorsPractice/Modificators.cs
+++ Sources/ModificatorsPractice/Modificators.cs
@@ -6,5 +6,11 @@ namespace csharp_practice.Sources.ModificatorsPractice
         public string Prop1 { get; set; }
         private string Prop2 { get; set; }
         protected string Prop3 { get; set; }
+
+        public Modificators(string prop1, string prop2, string prop3) {
+            this.Prop1 = prop1;
+            this.Prop2 = prop2;
+            this.Prop3 = prop3;
+        }
     }
 }
