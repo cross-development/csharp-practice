d0d53ea176a7efde51425b55ae2a1f6e3c07c770
Vitalii Derda <derdavitaliy@gmail.com>
2022-11-16 09:43:38 +0200

    added practice with modificators

---
+++ Sources/ModificatorsPractice/Modificators.cs
@@ -0,0 +1,10 @@
+
+namespace csharp_practice.Sources.ModificatorsPractice
+{
+    public class Modificators
+    {
+        public string Prop1 { get; set; }
+        private string Prop2 { get; set; }
+        protected string Prop3 { get; set; }
+    }
+}
